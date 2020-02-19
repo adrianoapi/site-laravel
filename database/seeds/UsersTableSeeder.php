bf43749a2be6ea13d84827a7f1051a8f73b6ed31
Adriano Araujo Costa <adriano@it4d.com.br>
2020-02-19 10:13:50 -0300

    update seed user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,24 +14,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $name = $this->makeRandomString(rand(5,10));
-        User::create([
-            'name'      => $name,
-            'email'     => $name.'@hotmail.com',
-            'password'  =>  Hash::make('123'),
-        ]);
-    }
-
-    private function makeRandomString($max=6) {
-        $i = 0; //Reset the counter.
-        $possible_keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        $keys_length = strlen($possible_keys);
-        $str = ""; //Let's declare the string, to add later.
-        while($i<$max) {
-            $rand = mt_rand(1,$keys_length-1);
-            $str.= $possible_keys[$rand];
-            $i++;
-        }
-        return $str;
+        factory(User::class,10)->create();
     }
 }

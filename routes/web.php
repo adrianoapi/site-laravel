7e850bc514c38a0d4a6463886253caceb3ffc242
Adriano A Costa <adrianoapi@hotmail.com>
2020-02-19 18:37:37 -0300

    update routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('produtos', 'Form\\ProductController')->names('products')->parameters(['produtos' => 'product']);
+
 Route::get('listagem', 'UserController@listUser');
 Route::group(['namespace' => 'form'], function () {
     /**

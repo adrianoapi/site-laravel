93cee2f0957e5fa1b1686c295515899716be0873
Adriano A Costa <adrianoapi@hotmail.com>
2020-03-12 15:31:50 -0300

    add layout admin

--- resources/views/admin/formLogin.blade.php
+++ resources/views/admin/formLogin.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Admin Login</title>
-</head>
-<body>
+@extends('admin.master.layout')
+
+@section('content')
     <h1>Login</h1>
     <form action="{{route('admin.login.do')}}" action="" method="post">
         @csrf
@@ -24,5 +18,4 @@
         <br>
         <input type="submit" value="Autenticar">
     </form>
-</body>
-</html>
+@endsection

43495e2c967505309cea6ab19fbc75ce70c6818c
Adriano API <adrianoapi@hotmail.com>
2020-05-13 19:14:02 -0300

    addlinks table

---
+++ database/migrations/2020_05_13_214227_create_links_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('link_id');
+            $table->string('title');
+            $table->timestamps();
+        });
+
+        Schema::table('links', function (Blueprint $table) 
+        {
+            $table->foreign('link_id')->references('id')->on('links')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}

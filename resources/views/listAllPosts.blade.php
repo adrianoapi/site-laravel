523563f8775fc0e89fdf800a2c55324369f637e7
Adriano API <adrianoapi@hotmail.com>
2020-05-24 22:13:13 -0300

    update form post list

--- resources/views/listAllPosts.blade.php
+++ resources/views/listAllPosts.blade.php
@@ -1,49 +1,66 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Listagem de Posts</title>
-</head>
-<body>
-    <a href="{{route('posts.create')}}">Novo</a>
-    <table border="1">
-        <thead>
-            <tr>
-                <td>id</td>
-                <td>name</td>
-                <td>email</td>
-                <td>actions</td>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($posts as $post)
-            <tr>
-                <td>{{$post->id}}</td>
-                <td>{{$post->title}}</td>
-                <td>{{$post->subtitle}}</td>
-                <td>
-                    <form action="{{route('posts.destroy', ['post' => $post->id])}}" method="POST">
-                        @csrf
-                        @method('delete')
-                        <input type="hidden" name="post" value="{{$post->id}}">
-                        <input type="submit" value="Excluir">
-                    </form>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-        <tfoot>
-            <tr>
-                <td colspan="4">{{ $posts->fragment('foo')->links() }}</td>
-            </tr>
-        </tfoot>
-    </table>
+@extends('dashboard.master.layout')
 
-    <style>li {display: inline;}</style>
+@section('content')
 
-      
+<div class="container-fluid">
+    <div class="page-header">
+        <div class="pull-left">
+            <h1>Favortio Item</h1>
+        </div>
+        <div class="pull-right">
+            <div class="btn-toolbar">
+                <a href="{{route('posts.create')}}" class="btn btn-primary"><i class="icon-plus" title="Adicionar"></i> Adicionar</a>
+            </div> 
+        </div> 
+    </div>
+    <div class="box box-color box-bordered">
+        <div class="box-title">
+            <h3>
+                <i class="icon-table"></i>
+                Listagem
+            </h3>
+        </div>
+        <div class="box-content nopadding">
+            <table class="table table-hover table-nomargin">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Options</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($posts as $post)
+                        <tr>
+                            <td>{{$post->id}}</td>
+                            <td>{{$post->title}}</td>
+                            <td>
+                                <form action="{{route('posts.destroy', ['post' => $post->id])}}" method="POST" onSubmit="return confirm('Deseja excluir?');" style="padding: 0px;margin:0px;">
+                                    @csrf
+                                    @method('delete')
+                                     <input type="hidden" name="post" value="{{$post->id}}">
+                                    <button type="submit" class="btn" rel="tooltip" title="" data-original-title="Excluir"><i class="icon-trash"></i></button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            {{ $posts->fragment('foo')->links() }}
+            <style>li {display: inline;}</style>
+            <div class="table-pagination">
+                <a href="#" class="disabled">First</a>
+                <a href="#" class="disabled">Previous</a>
+                <span>
+                    <a href="#" class="active">1</a>
+                    <a href="#">2</a>
+                    <a href="#">3</a>
+                </span>
+                <a href="#">Next</a>
+                <a href="#">Last</a>
+            </div>
+        </div>
+    </div>
+</div>
     
-</body>
-</html>
+@endsection

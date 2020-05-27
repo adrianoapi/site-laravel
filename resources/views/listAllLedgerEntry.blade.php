0819636f8a82926f40be03cecfa4f679b5939045
Adriano API <adrianoapi@hotmail.com>
2020-05-27 15:22:07 -0300

    add brand

--- resources/views/listAllLedgerEntry.blade.php
+++ resources/views/listAllLedgerEntry.blade.php
@@ -2,6 +2,26 @@
 
 @section('content')
 
+<br>
+<div class="breadcrumbs">
+    <ul>
+        <li>
+            <a href="more-login.html">Home</a>
+            <i class="icon-angle-right"></i>
+        </li>
+        <li>
+            <a href="components-messages.html">Components</a>
+            <i class="icon-angle-right"></i>
+        </li>
+        <li>
+            <a href="components-bootstrap.html">Bootstrap elements</a>
+        </li>
+    </ul>
+    <div class="close-bread">
+        <a href="#"><i class="icon-remove"></i></a>
+    </div>
+</div>
+
 <div class="container-fluid">
 
     <div class="box box-bordered">

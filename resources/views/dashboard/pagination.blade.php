32f5043fbfb9ff78a17818e62b806075bb3a7f5b
Adriano API <adrianoapi@hotmail.com>
2020-05-27 12:42:52 -0300

    add param in list page

--- resources/views/dashboard/pagination.blade.php
+++ resources/views/dashboard/pagination.blade.php
@@ -4,7 +4,7 @@
         @if ($paginator->onFirstPage())
             <a href="#" class="disabled">Previous</a>
         @else
-            <a href="{{ $paginator->previousPageUrl() }}">Previous</a>
+            <a href="{{ $paginator->previousPageUrl().'&'.str_replace('page=', '', $_SERVER['QUERY_STRING']) }}">Previous</a>
         @endif
 
         {{-- Pagination Elements --}}
@@ -21,7 +21,7 @@
                     @if ($page == $paginator->currentPage())
                         <a href="#" class="active"><span>{{ $page }}</span></a>
                     @else
-                        <a href="{{ $url }}">{{ $page }}</a>
+                        <a href="{{ $url.'&'.str_replace('page=', '', $_SERVER['QUERY_STRING']) }}">{{ $page }}</a>
                     @endif
                 </span>
                 @endforeach
@@ -30,7 +30,7 @@
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <a href="{{ $paginator->nextPageUrl() }}" rel="next">Next</a>
+            <a href="{{ $paginator->nextPageUrl().'&'.str_replace('page=', '', $_SERVER['QUERY_STRING']) }}" rel="next">Next</a>
         @else
             <a href="#" class="disabled">Next</a>
         @endif

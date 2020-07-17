062b1aee7a9853200e2d992d3d7795712bafafa2
Adriano A Costa <adrianoapi@hotmail.com>
2020-07-17 08:50:23 -0400

    add columns to collections table

---
+++ database/migrations/2020_07_16_220026_add_columns_to_config_collections.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToConfigCollections extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('collections', function (Blueprint $table) {
+            $table->boolean('show_title');
+            $table->boolean('show_description');
+            $table->boolean('show_release');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('collections', function (Blueprint $table) {
+            $table->dropColumn('show_title');
+            $table->dropColumn('show_description');
+            $table->dropColumn('show_release');
+        });
+    }
+}

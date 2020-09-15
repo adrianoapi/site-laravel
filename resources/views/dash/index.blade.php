60ab1128a0b8d7779c3ac987545149e4ff8da0f5
Adriano <adrianoapi@hotmail.com>
2020-09-15 17:34:35 -0300

    chart adjust

--- resources/views/dash/index.blade.php
+++ resources/views/dash/index.blade.php
@@ -30,7 +30,7 @@
 
 <div class="wrapper wrapper-content">
         <div class="row">
-            <div class="col-lg-8">
+            <div class="col-lg-12">
                 <div class="ibox ">
                     <div class="ibox-content">
                         <div>
@@ -88,66 +88,6 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4">
-                <div class="ibox ">
-                    <div class="ibox-title">
-                        <span class="label label-warning float-right">Data has changed</span>
-                        <h5>User activity</h5>
-                    </div>
-                    <div class="ibox-content">
-                        <div class="row">
-                            <div class="col-4">
-                                <small class="stats-label">Pages / Visit</small>
-                                <h4>236 321.80</h4>
-                            </div>
-
-                            <div class="col-4">
-                                <small class="stats-label">% New Visits</small>
-                                <h4>46.11%</h4>
-                            </div>
-                            <div class="col-4">
-                                <small class="stats-label">Last week</small>
-                                <h4>432.021</h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="ibox-content">
-                        <div class="row">
-                            <div class="col-4">
-                                <small class="stats-label">Pages / Visit</small>
-                                <h4>643 321.10</h4>
-                            </div>
-
-                            <div class="col-4">
-                                <small class="stats-label">% New Visits</small>
-                                <h4>92.43%</h4>
-                            </div>
-                            <div class="col-4">
-                                <small class="stats-label">Last week</small>
-                                <h4>564.554</h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="ibox-content">
-                        <div class="row">
-                            <div class="col-4">
-                                <small class="stats-label">Pages / Visit</small>
-                                <h4>436 547.20</h4>
-                            </div>
-
-                            <div class="col-4">
-                                <small class="stats-label">% New Visits</small>
-                                <h4>150.23%</h4>
-                            </div>
-                            <div class="col-4">
-                                <small class="stats-label">Last week</small>
-                                <h4>124.990</h4>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
         </div>
 
         <div class="row">

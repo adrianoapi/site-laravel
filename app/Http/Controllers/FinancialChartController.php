3f9d42b43c8cd8252b5c69246cee92504429a340
Adriano <adrianoapi@hotmail.com>
2020-09-30 09:46:05 -0300

    add monthy calc in financial chart

--- app/Http/Controllers/FinancialChartController.php
+++ app/Http/Controllers/FinancialChartController.php
@@ -7,9 +7,16 @@ use Illuminate\Support\Facades\DB;
 
 class FinancialChartController extends Controller
 {
+    private $date_begin;
+    private $date_end;
+
     public function __construct()
     {
         $this->middleware('auth');
+
+        $this->date_begin = date('Y-m-d');
+        $this->date_begin = date('Y-m-d', strtotime("$this->date_begin -30 days"));
+        $this->date_end   = date('Y-m-d');
     }
 
     public function index()
@@ -19,12 +26,52 @@ class FinancialChartController extends Controller
         ->join('transition_types', 'ledger_entries.transition_type_id', '=', 'transition_types.id')
         ->where([
             ['transition_types.action', '=', 'expensive'],
+            ['ledger_entries.entry_date', '>=', $this->date_begin],
+            ['ledger_entries.entry_date', '<=', $this->date_end]
         ])
         ->orderByDesc('ledger_entries.amount', 'desc')
-        ->limit(10)
+        ->limit(3)
         ->get();
 
-        return view('financialChart.index', ['rank_cost' => $rank_cost]);
+        $monthlyExpense = DB::table('ledger_entries')
+        ->join('transition_types', 'ledger_entries.transition_type_id', '=', 'transition_types.id')
+        ->select(DB::raw('sum( ledger_entries.amount ) as total'))
+        ->where([
+            ['transition_types.action', '=', 'expensive'],
+            ['transition_types.credit_card', '<>', true],
+            ['ledger_entries.entry_date', '>=', $this->date_begin],
+            ['ledger_entries.entry_date', '<=', $this->date_end]
+        ])
+        ->orderByDesc('ledger_entries.entry_date')
+        ->get();
+        
+        $monthlyExpenseCart = DB::table('ledger_entries')
+        ->join('transition_types', 'ledger_entries.transition_type_id', '=', 'transition_types.id')
+        ->select(DB::raw('sum( ledger_entries.amount ) as total'))
+        ->where([
+            ['transition_types.action', '=', 'expensive'],
+            ['transition_types.credit_card', '=', true],
+            ['ledger_entries.entry_date', '>=', $this->date_begin],
+            ['ledger_entries.entry_date', '<=', $this->date_end]
+        ])
+        ->orderByDesc('ledger_entries.entry_date')
+        ->get();
+
+        $monthlyRecipe = DB::table('ledger_entries')
+        ->join('transition_types', 'ledger_entries.transition_type_id', '=', 'transition_types.id')
+        ->select(DB::raw('sum( ledger_entries.amount ) as total'))
+        ->where([
+            ['transition_types.action', '=', 'recipe'],
+            ['ledger_entries.entry_date', '>=', $this->date_begin],
+            ['ledger_entries.entry_date', '<=', $this->date_end]
+        ])
+        ->orderByDesc('ledger_entries.entry_date')
+        ->get();
+
+        return view('financialChart.index', [
+            'rank_cost' => $rank_cost,
+            'monthly'   => ['monthlyExpense' => $monthlyExpense, 'monthlyExpenseCart' => $monthlyExpenseCart, 'monthlyRecipe' => $monthlyRecipe]
+            ]);
     }
 
 }
--- resources/views/financialChart/index.blade.php
+++ resources/views/financialChart/index.blade.php
@@ -55,6 +55,63 @@
             </div>
         </div>
 
+        <div class="col-lg-6">
+            <div class="ibox ">
+                <div class="ibox-title">
+                    <h5>Em 30 dias: Cartão <span class="label label-warning">R$ {{$monthly['monthlyExpenseCart'][0]->total}}</span></h5>
+                    <div class="ibox-tools">
+                        <a class="collapse-link">
+                            <i class="fa fa-chevron-up"></i>
+                        </a>
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                            <i class="fa fa-wrench"></i>
+                        </a>
+                        <ul class="dropdown-menu dropdown-user">
+                            <li><a href="#" class="dropdown-item">Config option 1</a>
+                            </li>
+                            <li><a href="#" class="dropdown-item">Config option 2</a>
+                            </li>
+                        </ul>
+                        <a class="close-link">
+                            <i class="fa fa-times"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="ibox-content">
+                    
+                    <table class="table table-hover margin bottom">
+                        <thead>
+                        <tr>
+                            <th class="">Date</th>
+                            <th class="">Amount</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        <?php
+                            $total = $monthly['monthlyRecipe'][0]->total - $monthly['monthlyExpense'][0]->total;
+                        ?>
+
+                            <tr>
+                                <td>Débito</td>
+                                <td><span class="label label-danger">{{$monthly['monthlyExpense'][0]->total}}</span></td>
+                            </tr>
+                            <tr>
+                                <td>Cartão</td>
+                                <td><span class="label label-info">{{$monthly['monthlyRecipe'][0]->total}}</span></td>
+                            </tr>
+                            <tr>
+                                <td>Total</td>
+                                <td><span class="label label-{{$total > 0 ? 'info': 'danger'}}">{{$total}}</span></td>
+                            </tr>
+                        
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
 

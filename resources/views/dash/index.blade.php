b8feadfb21a0c564f981c109d4eaaa7a26b3e6db
Adriano API <adrianoapi@hotmail.com>
2020-10-07 21:41:08 -0300

    add butons chart

--- resources/views/dash/index.blade.php
+++ resources/views/dash/index.blade.php
@@ -5,65 +5,76 @@
 @section('content')
 
 <div class="wrapper wrapper-content">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="ibox ">
-                    <div class="ibox-content">
 
-                        <div class="m-t-sm">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="ibox ">
+                <div class="ibox-title">
+                    <h5>Orders</h5>
+                    <div class="float-right">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-xs btn-white active">Today</button>
+                            <button type="button" class="btn btn-xs btn-white">Monthly</button>
+                            <button type="button" class="btn btn-xs btn-white">Annual</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="ibox-content">
 
-                            <div class="row">
-                                <div class="col-md-8" id="ajax-chart">
-                                    <div class="ibox-content">
-                                        <div class="spiner-example">
-                                            <div class="sk-spinner sk-spinner-wave">
-                                                <div class="sk-rect1"></div>
-                                                <div class="sk-rect2"></div>
-                                                <div class="sk-rect3"></div>
-                                                <div class="sk-rect4"></div>
-                                                <div class="sk-rect5"></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-4" id="ajax-finance">
-                                    <div class="ibox-content">
-                                        <div class="spiner-example">
-                                            <div class="sk-spinner sk-spinner-wave">
-                                                <div class="sk-rect1"></div>
-                                                <div class="sk-rect2"></div>
-                                                <div class="sk-rect3"></div>
-                                                <div class="sk-rect4"></div>
-                                                <div class="sk-rect5"></div>
-                                            </div>
+                    <div class="m-t-sm">
+
+                        <div class="row">
+                            <div class="col-md-8" id="ajax-chart">
+                                <div class="ibox-content">
+                                    <div class="spiner-example">
+                                        <div class="sk-spinner sk-spinner-wave">
+                                            <div class="sk-rect1"></div>
+                                            <div class="sk-rect2"></div>
+                                            <div class="sk-rect3"></div>
+                                            <div class="sk-rect4"></div>
+                                            <div class="sk-rect5"></div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-
+                            <div class="col-md-4" id="ajax-finance">
+                                <div class="ibox-content">
+                                    <div class="spiner-example">
+                                        <div class="sk-spinner sk-spinner-wave">
+                                            <div class="sk-rect1"></div>
+                                            <div class="sk-rect2"></div>
+                                            <div class="sk-rect3"></div>
+                                            <div class="sk-rect4"></div>
+                                            <div class="sk-rect5"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
 
                     </div>
+
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="row" id="ajax-task">
-            <div class="ibox-content">
-                <div class="spiner-example">
-                    <div class="sk-spinner sk-spinner-wave">
-                        <div class="sk-rect1"></div>
-                        <div class="sk-rect2"></div>
-                        <div class="sk-rect3"></div>
-                        <div class="sk-rect4"></div>
-                        <div class="sk-rect5"></div>
-                    </div>
+    <div class="row" id="ajax-task">
+        <div class="ibox-content">
+            <div class="spiner-example">
+                <div class="sk-spinner sk-spinner-wave">
+                    <div class="sk-rect1"></div>
+                    <div class="sk-rect2"></div>
+                    <div class="sk-rect3"></div>
+                    <div class="sk-rect4"></div>
+                    <div class="sk-rect5"></div>
                 </div>
             </div>
         </div>
+    </div>
 
 
-        </div>
+</div>
 
 
 @endsection

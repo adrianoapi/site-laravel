3cd05ad8aa0cf33adf7f3f657c526c7b5633393d
Adriano <adrianoapi@hotmail.com>
2020-11-10 17:51:18 -0300

    pagination update

--- resources/views/dashboard/pagination.blade.php
+++ resources/views/dashboard/pagination.blade.php
@@ -1,38 +1,38 @@
 @if ($paginator->hasPages())
-    <div class="table-pagination">
+    <ul class="pagination float-right">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <a href="#" class="disabled">Previous</a>
+        <li class="footable-page-arrow"><a href="#" class="disabled"><</a></li>
         @else
-            <a href="{{ $paginator->previousPageUrl().'&'.str_replace('page=', '', $_SERVER['QUERY_STRING']) }}">Previous</a>
+        <li class="footable-page-arrow"><a href="{{ $paginator->previousPageUrl().'&'.str_replace('page=', '', $_SERVER['QUERY_STRING']) }}"><</a></li>
         @endif
 
         {{-- Pagination Elements --}}
         @foreach ($elements as $element)
             {{-- "Three Dots" Separator --}}
             @if (is_string($element))
-                <li class="disabled"><span>{{ $element }}</span></li>
+                <li class="footable-page"><span>{{ $element }}</span></li>
             @endif
 
             {{-- Array Of Links --}}
             @if (is_array($element))
                 @foreach ($element as $page => $url)
-                <span>
+
                     @if ($page == $paginator->currentPage())
-                        <a href="#" class="active"><span>{{ $page }}</span></a>
+                    <li class="footable-page active"><a href="#" class="active"><span>{{ $page }}</span></a></li>
                     @else
-                        <a href="{{ $url.'&'.str_replace('page=', '', $_SERVER['QUERY_STRING']) }}">{{ $page }}</a>
+                    <li class="footable-page"><a href="{{ $url.'&'.str_replace('page=', '', $_SERVER['QUERY_STRING']) }}">{{ $page }}</a></li>
                     @endif
-                </span>
+
                 @endforeach
             @endif
         @endforeach
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <a href="{{ $paginator->nextPageUrl().'&'.str_replace('page=', '', $_SERVER['QUERY_STRING']) }}" rel="next">Next</a>
+        <li class="footable-page-arrow"><a href="{{ $paginator->nextPageUrl().'&'.str_replace('page=', '', $_SERVER['QUERY_STRING']) }}" rel="next">></a></li>
         @else
-            <a href="#" class="disabled">Next</a>
+        <li class="footable-page-arrow"><a href="#" class="disabled">></a></li>
         @endif
-    </div>
+    </ul>
 @endif

25db97a5f5e9e87eeaf355f95a9e5f36b413d093
Adriano <adrianoapi@hotmail.com>
2021-02-01 10:19:59 -0300

    fixed layout items

--- resources/views/collectionItem/show.blade.php
+++ resources/views/collectionItem/show.blade.php
@@ -5,6 +5,7 @@
 @section('content')
 
     <div class="wrapper wrapper-content animated fadeInRight">
+
         <div class="row">
             <div class="col-lg-12">
                 <div class="ibox ">
@@ -13,21 +14,17 @@
                     </div>
                     <div class="ibox-content">
                         <div class="row">
-                            <ul class="tabs actions">
-                                <li>
-                                    <a href="{{route('collItems.create', ['collection' => $collection->id])}}" data-toggle="modal" class="btn"><i class="icon-edit"></i> Novo Item</a>
-                                </li>
-                                <li class="active">
-                                    <a href="{{route('collItems.show',   ['collection'  => $collection->id])}}" data-toggle="modal" class="btn"><i class="glyphicon-tags"></i> Itens</a>
-                                </li>
-                                <li>
-                                    <a href="{{route('collections.show', ['collection'   => $collection->id])}}" data-toggle="modal" class="btn"><i class="icon-search"></i> Coleção</a>
-                                </li>
-                            </ul>
+                            <div class="col-sm-6 m-b-xs">
+                                <a href="{{route('collItems.create', ['collection' => $collection->id])}}" class="btn btn-default"><i class="icon-edit"></i> Novo Item</a>
+                                <a href="{{route('collItems.show',   ['collection'  => $collection->id])}}" class="btn btn-info"><i class="icon-shopping-cart"></i> Itens</a>
+                                <a href="{{route('collections.show', ['collection'   => $collection->id])}}" class="btn btn-default"><i class="icon-reorder"></i> Coleção</a>
+                            </div>
+                            <div class="col-sm-6">
+
+                            </div>
                         </div>
+
                         <div class="table-responsive">
-
-
 
                             <div class="box-content nopadding">
                                 <table class="table table-hover table-nomargin">
@@ -63,9 +60,10 @@
                                                 <form action="{{route('collItems.destroy', ['collItem' => $value->id])}}" method="POST" onSubmit="return confirm('Deseja excluir?');" style="padding: 0px;margin:0px;">
                                                     @csrf
                                                     @method('delete')
-                                                    <a href="{{route('collItemImages.create', ['collItem'  => $value->id])}}" class="btn" rel="tooltip" title="" data-original-title="Imagem"><i class="glyphicon-picture"></i></a>
-                                                    <a href="{{route('collItems.edit',        ['collItem'  => $value->id])}}" class="btn" rel="tooltip" title="" data-original-title="Editar"><i class="icon-edit"></i> Editar</a>
-                                                    <button type="submit" class="btn btn-inverse"><i class="icon-trash"></i> Excluir</button>
+
+                                                    <a href="{{route('collItemImages.create', ['collItem'  => $value->id])}}" class="btn btn-white btn-sm" rel="tooltip" title="" data-original-title="Imagem"><i class="fa fa-image"></i></a>
+                                                    <a href="{{route('collItems.edit',        ['collItem'  => $value->id])}}" class="btn btn-white btn-sm" rel="tooltip" title="" data-original-title="Editar"><i class="fa fa-edit"></i></a>
+                                                    <button type="submit" class="btn btn-white btn-sm" rel="tooltip" title="" data-original-title="Excluir"><i class="fa fa-trash"></i></button>
                                                 </form>
                                             </td>
                                         </tr>
@@ -74,7 +72,7 @@
                                 </table>
                             </div>
 
-                            
+
                         </div>
                     </div>
 

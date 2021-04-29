7c108789df94a9a213e1af41712bc48f84f5d013
Adriano <adrianoapi@hotmail.com>
2021-04-29 11:20:49 -0300

    rollback transitionType edit template

--- resources/views/transitionType/edit.blade.php
+++ resources/views/transitionType/edit.blade.php
@@ -1,101 +1,56 @@
-@extends('layouts.app')
-
-@section('title', 'Transação tipo / editar')
+@extends('dashboard.master.layout')
 
 @section('content')
 
-<div class="wrapper wrapper-content  animated fadeInRight">
-
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="ibox ">
-                <div class="ibox-title">
-                    <h5>Transação tipo: editar</h5>
-                    <div class="ibox-tools">
-                        <a class="collapse-link">
-                            <i class="fa fa-chevron-up"></i>
-                        </a>
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                            <i class="fa fa-wrench"></i>
-                        </a>
-                        <ul class="dropdown-menu dropdown-user">
-                            <li><a href="#" class="dropdown-item">Config option 1</a>
-                            </li>
-                            <li><a href="#" class="dropdown-item">Config option 2</a>
-                            </li>
-                        </ul>
-                        <a class="close-link">
-                            <i class="fa fa-times"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="ibox-content">
-                    <form action="{{route('transitionTypes.update', ['transitionType' => $transitionType->id])}}" method="POST" class="form-horizontal form-bordered">
-                        @csrf
-                        @method('PUT')
-                        <div class="form-group  row"><label class="col-sm-2 col-form-label">Título</label>
-                            <div class="col-sm-10">
-                                <input type="text" name="title" id="title" value="{{$transitionType->title}}" class="form-control" tabindex="1">
-                            </div>
-                        </div>
-                        <div class="hr-line-dashed"></div>
-
-                        <div class="form-group  row"><label class="col-sm-2 col-form-label">Descrição</label>
-                            <div class="col-sm-10">
-                                <textarea id="content" name="description">{{$transitionType->description}}</textarea>
-                            </div>
-                        </div>
-                        <div class="hr-line-dashed"></div>
-
-                        <div class="form-group row"><label class="col-sm-2 col-form-label">Tipo de despesa <br/><small class="text-navy">Entrada/Saída de dinheiro</small></label>
-
-                            <div class="col-sm-10">
-                                <div class="i-checks"><label> <input type="radio" name="action" value="recipe"     {{ $transitionType->action === "recipe" ? "checked" : "" }}> <i></i> Receita</label></div>
-                                <div class="i-checks"><label> <input type="radio" name="action" value="expensive"  {{ $transitionType->action === "expensive" ? "checked" : "" }}> <i></i> Despesa </label></div>
-                                <div class="i-checks"><label> <input type="radio" name="action" value="investment" {{ $transitionType->action === "investment" ? "checked" : "" }}> <i></i> Investimento </label></div>
-                            </div>
-                        </div>
-                        <div class="hr-line-dashed"></div>
-
-                        <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">Cartão de crédito<br>
-                            <small>Define que será somado apenas na fatura</small></label>
-                            <div class="col-sm-10">
-                                <div class="i-checks"><label> <input type="checkbox" name="credit_card" id="credit_card" value="true" tabindex="6" {{$transitionType->credit_card == true ? 'checked' : ''}}> <i></i> Cartão de crédito </label></div>
-                            </div>
-                        </div>
-
-                        <div class="hr-line-dashed"></div>
-          
-                        <div class="form-group row">
-                            <div class="col-sm-4 col-sm-offset-2">
-                                <a href="{{route('transitionTypes.index')}}" class="btn btn-white btn-sm">Cancelar</a>
-                                <button class="btn btn-primary btn-sm" type="submit">Save </button>
-                            </div>
-                        </div>
-
-                    </form>
-                </div>
+<div class="container-fluid">
+    <div class="page-header">
+        <div class="pull-left">
+            <h1>/transacoes-tipo</h1>
+        </div>
+        <div class="pull-right">
+            <div class="btn-toolbar">
+                <a href="{{route('transitionTypes.index')}}" class="btn btn-primary"><i class="icon-reorder" title="Listagem"></i> Listagem</a>
             </div>
         </div>
     </div>
-
-
+    <div class="box box-bordered box-color">
+        <div class="box-title">
+            <h3><i class="icon-th-list"></i> Adicionar</h3>
+        </div>
+        <div class="box-content nopadding">
+            <form action="{{route('transitionTypes.update', ['transitionType' => $transitionType->id])}}" method="POST" class="form-horizontal form-bordered">
+                @csrf
+                @method('PUT')
+                <div class="control-group">
+                    <label for="title" class="control-label">Title</label>
+                    <div class="controls">
+                        <input type="text" name="title" id="title" value="{{$transitionType->title}}" placeholder="Text input" class="input-xlarge">
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label for="content" class="control-label">Conteúdo</label>
+                    <div class="controls">
+                        <textarea name="description" id="description" rows="5" class="input-block-level">{{$transitionType->description}}</textarea>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label">Radios</label>
+                    <div class="controls">
+                        <label class="radio">
+                            <input type="radio" name="action" value="expensive" {{ $transitionType->action === "expensive" ? "checked" : "" }}> Despesa
+                        </label>
+                        <label class="radio">
+                            <input type="radio" name="action" value="recipe" {{ $transitionType->action === "recipe" ? "checked" : "" }}> Receita
+                        </label>
+                    </div>
+                </div>
+                <div class="form-actions">
+                    <button type="submit" class="btn btn-primary">Salvar</button>
+                    <a href="{{route('transitionTypes.index')}}" class="btn">Cancelar</a>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
-        
-@endsection
 
-@section('scripts')
-<script>
-    $('.chosen-select').chosen({width: "100%"});
-    $(document).ready(function () {
-
-        $('#content').summernote();
-
-        $('.i-checks').iCheck({
-            checkboxClass: 'icheckbox_square-green',
-            radioClass: 'iradio_square-green',
-        });
-    });
-</script>
 @endsection

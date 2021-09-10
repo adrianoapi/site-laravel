b0130cb863ca695dca350c96ea1e4d66b3db1cf1
Adriano <adrianoapi@hotmail.com>
2021-09-10 17:21:21 -0300

    Rollback ledgerGroup template

--- resources/views/ledgerGroup/add.blade.php
+++ resources/views/ledgerGroup/add.blade.php
@@ -1,100 +1,63 @@
-@extends('layouts.app')
-
-@section('title', 'Lançamentos grupo: Cadastrar')
+@extends('dashboard.master.layout')
 
 @section('content')
 
-<div class="wrapper wrapper-content  animated fadeInRight">
-
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="ibox ">
-                <div class="ibox-title">
-                    <h5>Lançamentos grupo: Cadastrar</h5>
-                    <div class="ibox-tools">
-                        <a class="collapse-link">
-                            <i class="fa fa-chevron-up"></i>
-                        </a>
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                            <i class="fa fa-wrench"></i>
-                        </a>
-                        <ul class="dropdown-menu dropdown-user">
-                            <li><a href="#" class="dropdown-item">Config option 1</a>
-                            </li>
-                            <li><a href="#" class="dropdown-item">Config option 2</a>
-                            </li>
-                        </ul>
-                        <a class="close-link">
-                            <i class="fa fa-times"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="ibox-content">
-                    <form action="{{route('ledgerGroups.store')}}" method="POST" class="form-horizontal form-bordered">
-                        @csrf
-                        <div class="form-group row {{$errors->has('title') ? 'error' : ''}}"><label class="col-sm-2 col-form-label">Título</label>
-                            <div class="col-sm-10">
-                                <input type="text" name="title" id="title" value="" class="form-control" tabindex="1">
-                                @if ($errors->has('title'))
-                                    <span class="help-inline">{{ $errors->first('title') }}</span>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="hr-line-dashed"></div>
-
-                        <div class="form-group row {{$errors->has('ledger_group_id') ? 'error' : ''}}">
-                            <label class="col-sm-2 col-form-label">Level</label>
-                            <div class="col-sm-10">
-                                <select name="ledger_group_id" id="ledger_group_id" data-placeholder="Choose a level..." class="chosen-select"  tabindex="2">
-                                    <option value="">Select...</option>
-                                    @foreach ($parents as $value)
-                                        <option value="{{$value->id}}">{{$value->title}}</option>
-                                    @endforeach
-                                </select>
-                                @if ($errors->has('ledger_group_id'))
-                                    <span class="help-inline">{{ $errors->first('ledger_group_id') }}</span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="hr-line-dashed"></div>
-
-                        <div class="form-group row"><label class="col-sm-2 col-form-label">Descrição</label>
-                            <div class="col-sm-10">
-                                <textarea id="content" name="description"></textarea>
-                            </div>
-                        </div>
-                        <div class="hr-line-dashed"></div>
-
-                        <div class="form-group row">
-                            <div class="col-sm-4 col-sm-offset-2">
-                                <a href="{{route('ledgerGroups.index')}}" class="btn btn-white btn-sm">Cancelar</a>
-                                <button class="btn btn-primary btn-sm" type="submit">Save </button>
-                            </div>
-                        </div>
-
-                    </form>
-                </div>
+<div class="container-fluid">
+    <div class="page-header">
+        <div class="pull-left">
+            <h1>/lancamento-grupo</h1>
+        </div>
+        <div class="pull-right">
+            <div class="btn-toolbar">
+                <a href="{{route('ledgerGroups.index')}}" class="btn btn-primary"><i class="icon-reorder" title="Listagem"></i> Listagem</a>
             </div>
         </div>
     </div>
+    <div class="box box-bordered box-color">
+        <div class="box-title">
+            <h3><i class="icon-th-list"></i> Adicionar</h3>
+        </div>
+        <div class="box-content nopadding">
+            <form action="{{route('ledgerGroups.store')}}" method="POST" class="form-horizontal form-bordered">
+                @csrf
+                <div class="control-group {{$errors->has('title') ? 'error' : ''}}">
+                    <label for="title" class="control-label">Title</label>
+                    <div class="controls">
+                        <input type="text" name="title" id="title" placeholder="Text input" class="input-xlarge">
+                        @if ($errors->has('title'))
+                            <span class="help-inline">{{ $errors->first('title') }}</span>
+                        @endif
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label for="ledger_groups_id" class="control-label">Basic</label>
+                    <div class="controls">
+                        <select name="ledger_groups_id" id="ledger_groups_id" class="select2-me input-xlarge">
+                            <option value="">Select...</option>
+                            @foreach ($parents as $value)
+                                <option value="{{$value->id}}">{{$value->title}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label for="description" class="control-label">Conteúdo</label>
+                    <div class="controls">
+                        <textarea name="description" id="description" rows="5" class="input-block-level"></textarea>
+                    </div>
+                </div>
 
-
+                <div class="form-actions">
+                    <button type="submit" class="btn btn-primary">Salvar</button>
+                    <a href="{{route('ledgerGroups.index')}}" class="btn">Cancelar</a>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
 
 @endsection
 
 @section('scripts')
-<script>
-    $('.chosen-select').chosen({width: "100%"});
-    $(document).ready(function () {
 
-        $('#content').summernote();
-
-        $('.i-checks').iCheck({
-            checkboxClass: 'icheckbox_square-green',
-            radioClass: 'iradio_square-green',
-        });
-    });
-</script>
 @endsection
--- resources/views/ledgerGroup/edit.blade.php
+++ resources/views/ledgerGroup/edit.blade.php
@@ -1,98 +1,65 @@
-@extends('layouts.app')
-
-@section('title', 'Lançamentos grupo: Editar')
+@extends('dashboard.master.layout')
 
 @section('content')
 
-<div class="wrapper wrapper-content  animated fadeInRight">
-
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="ibox ">
-                <div class="ibox-title">
-                    <h5>Lançamentos grupo: Editar</h5>
-                    <div class="ibox-tools">
-                        <a class="collapse-link">
-                            <i class="fa fa-chevron-up"></i>
-                        </a>
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                            <i class="fa fa-wrench"></i>
-                        </a>
-                        <ul class="dropdown-menu dropdown-user">
-                            <li><a href="#" class="dropdown-item">Config option 1</a>
-                            </li>
-                            <li><a href="#" class="dropdown-item">Config option 2</a>
-                            </li>
-                        </ul>
-                        <a class="close-link">
-                            <i class="fa fa-times"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="ibox-content">
-                    <form action="{{route('ledgerGroups.update', ['ledgerGroup' => $ledgerGroup->id])}}" method="POST" class="form-horizontal form-bordered">
-                        @csrf
-                        @method('PUT')
-                        <div class="form-group row {{$errors->has('title') ? 'error' : ''}}"><label class="col-sm-2 col-form-label">Título</label>
-                            <div class="col-sm-10">
-                                <input type="text" name="title" id="title" value="{{$ledgerGroup->title}}" class="form-control" tabindex="1">
-                                @if ($errors->has('title'))
-                                    <span class="help-inline">{{ $errors->first('title') }}</span>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="hr-line-dashed"></div>
-
-                        <div class="form-group  row">
-                            <label class="col-sm-2 col-form-label">Level</label>
-                            <div class="col-sm-10">
-                                <select name="ledger_group_id" id="ledger_group_id" data-placeholder="Choose a level..." class="chosen-select"  tabindex="2">
-                                    <option value="">Select...</option>
-                                    @foreach ($parents as $value)
-                                        <option value="{{$value->id}}" {{$value->id == $ledgerGroup->ledger_group_id ? 'selected' : ''}}>{{$value->title}}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-
-                        <div class="hr-line-dashed"></div>
-
-                        <div class="form-group row"><label class="col-sm-2 col-form-label">Descrição</label>
-                            <div class="col-sm-10">
-                                <textarea id="content" name="description">{{$ledgerGroup->description}}</textarea>
-                            </div>
-                        </div>
-                        <div class="hr-line-dashed"></div>
-
-                        <div class="form-group row">
-                            <div class="col-sm-4 col-sm-offset-2">
-                                <a href="{{route('ledgerGroups.index')}}" class="btn btn-white btn-sm">Cancelar</a>
-                                <button class="btn btn-primary btn-sm" type="submit">Save </button>
-                            </div>
-                        </div>
-
-                    </form>
-                </div>
+<div class="container-fluid">
+    <div class="page-header">
+        <div class="pull-left">
+            <h1>/lancamento-grupo</h1>
+        </div>
+        <div class="pull-right">
+            <div class="btn-toolbar">
+                <a href="{{route('ledgerGroups.index')}}" class="btn btn-primary"><i class="icon-reorder" title="Listagem"></i> Listagem</a>
             </div>
         </div>
     </div>
+    <div class="box box-bordered box-color">
+        <div class="box-title">
+            <h3><i class="icon-th-list"></i> Adicionar</h3>
+        </div>
+        <div class="box-content nopadding">
+            <form action="{{route('ledgerGroups.edit', ['ledgerGroup' => $ledgerGroup->id])}}" method="POST" class="form-horizontal form-bordered">
+                @csrf
+                @method('PUT')
+                <div class="control-group {{$errors->has('title') ? 'error' : ''}}">
+                    <label for="title" class="control-label">Title</label>
+                    <div class="controls">
+                        <input type="text" name="title" id="title" value="{{$ledgerGroup->title}}" placeholder="Text input" class="input-xlarge">
+                        @if ($errors->has('title'))
+                            <span class="help-inline">{{ $errors->first('title') }}</span>
+                        @endif
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label for="ledger_groups_id" class="control-label">Basic</label>
+                    <div class="controls">
+                        <select name="ledger_groups_id" id="ledger_groups_id" class="select2-me input-xlarge">
+                            <option value="">Select...</option>
+                            @foreach ($parents as $value)
+                                <option value="{{$value->id}}" {{$value->id == $ledgerGroup->ledger_groups_id ? 'selected' : ''}}>{{$value->title}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label for="description" class="control-label">Conteúdo</label>
+                    <div class="controls">
+                        <textarea name="description" id="description" rows="5" class="input-block-level">{{$ledgerGroup->description}}</textarea>
+                    </div>
+                </div>
 
-
+                <div class="form-actions">
+                    <button type="submit" class="btn btn-primary">Salvar</button>
+                    <a href="{{route('ledgerGroups.index')}}" class="btn">Cancelar</a>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
+
 
 @endsection
 
 @section('scripts')
-<script>
-    $('.chosen-select').chosen({width: "100%"});
-    $(document).ready(function () {
 
-        $('#content').summernote();
-
-        $('.i-checks').iCheck({
-            checkboxClass: 'icheckbox_square-green',
-            radioClass: 'iradio_square-green',
-        });
-    });
-</script>
 @endsection
